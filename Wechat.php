5b047cd583677a35844bfbed85264f7ccd62cff9
古俊杰 <dggugjj@163.com>
2014-08-28 16:46:41 +0800

    init

---
+++ Wechat.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 俊杰
+ * Date: 14-8-28
+ * Time: 下午3:56
+ */
+
+namespace yii\wechat;
+
+use yii\base\Component;
+
+class Wechat extends Component
+{
+
+} 
---
+++ module.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace yii\wechat;
+
+class module extends \yii\base\Module
+{
+    public $controllerNamespace = 'yii\wechat\controllers';
+
+    public function init()
+    {
+        parent::init();
+    }
+}
