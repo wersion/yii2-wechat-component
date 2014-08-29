30aa49798ae9d18d97236a3603c8691c008adc95
古俊杰 <dggugjj@163.com>
2014-08-29 09:52:05 +0800

    更新

--- Wechat.php
+++ Wechat.php
@@ -12,5 +12,33 @@ use yii\base\Component;
 
 class Wechat extends Component
 {
+    const MENU = '';
+
+    public function getAccessToken()
+    {
+
+    }
+
+    public function arrayToXml($array)
+    {
+        $xml = "<xml>";
+        foreach ($array as $key => $val) {
+            $xml .= "<" . $key . "><![CDATA[" . $val . "]]></" . $key . ">";
+        }
+        $xml .= "</xml>";
+        return $xml;
+    }
+
+    public static function addTokenToData($data)
+    {
+        $data['access_token'] = GetAccessToken::get();
+        return $data;
+    }
+
+    public static function addTokenToUrl($url)
+    {
+        return $url . "?access_token=" . GetAccessToken::action();
+    }
+
 
 } 
